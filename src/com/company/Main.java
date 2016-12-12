76e6076847f6c0f46a52badaadb50ae77ffed7bb
HuangHengyi <henrymakaay@gmail.com>
2016-12-12 10:43:40 +0800

    显示词法分析的详细结果，输出每个词对应的类型名字

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ package com.company;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.io.*;
+import java.util.List;
 
 
 public class Main {
@@ -17,11 +18,29 @@ public class Main {
         MinijavaParser parser = new MinijavaParser(tokens);
         ParseTree tree = parser.goal();
 
-        for (Object obj : tokens.getTokens()) System.out.println(obj);
+
+        Vocabulary Voca = lexer.getVocabulary();
+        //System.out.println(Voca.getDisplayName(1));
+
+        //List<Token> TokensList = tokens.getTokens();
+
+
+        for (Token obj : tokens.getTokens()) {
+            System.out.print(obj);
+            System.out.print(" || Type : ");
+     /*       System.out.print(" number ");
+            int ind = obj.getType();
+            System.out.print(ind);
+            System.out.print("   ");*/
+            System.out.println(Voca.getDisplayName(obj.getType()));
+
+        }
 
         System.out.println("LISP:");
         System.out.println(tree.toStringTree(parser));
         System.out.println();
+
+
 
     /*    System.out.println("Visitor:");
         EvalVisitor evalByVisitor = new EvalVisitor();
